168cf2f1b41a1f03beeabcde3522305c8fde0f41
rahuls915 <rahul.salunke@adypu.edu.in>
2023-09-28 08:30:28 +0530

    first ReterofitRecyclerView

---
+++ app/src/main/java/com/Rahul/ReterofitRecyclerView/Posts.java
@@ -0,0 +1,26 @@
+package com.Rahul.ReterofitRecyclerView;
+
+public class Posts {
+    private String title , body ;
+
+    public Posts(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
---
+++ app/src/main/java/com/Rahul/ReterofitRecyclerView/RetrofitClient.java
@@ -0,0 +1,23 @@
+package com.Rahul.ReterofitRecyclerView;
+
+import java.security.PrivateKey;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    private static Retrofit retrofit = null;
+
+    static ApiInterface getRetrofitClient(){
+        if(retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(ApiInterface.class);
+
+    }
+}
